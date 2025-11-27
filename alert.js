4f2321749e5845984f965fe7d89c8b5f2940ffee
Tawan67 <tawankhuntha@gmail.com>
2025-11-27 23:20:14 +0700

    we have to start database and backend

--- alert.js
+++ alert.js
@@ -1,3 +1,17 @@
 function sayHi() {
     alert("Hello from JavaScript!");
 }
+
+
+function sayHi(){
+    var name_full = "Dew" //ห้ามใช้ตัวแปรชื่อ name เพราะค่ามันไปชนกับ window.name จะทำให้ type มันเพี้ยนได้
+    for (let i=0;i<2;i++){
+        console.log("Hello"+name_full)
+        alert("Hi"+(i+1)+name_full)
+    }
+    name_full = 900
+    alert(typeof(name_full))
+    name_full = name_full + 1
+    console.log(name_full)
+    console.log(typeof(name_full))
+}
--- gotoPage.js
+++ gotoPage.js
@@ -1,6 +1,16 @@
 function gotoPage(link){
     window.location.href = link;
 }
-function goPara(link,para){
 
+function sayHi(){
+    var name_full = "Dew" //ห้ามใช้ตัวแปรชื่อ name เพราะค่ามันไปชนกับ window.name จะทำให้ type มันเพี้ยนได้
+    for (let i=0;i<2;i++){
+        console.log("Hello"+name_full)
+        alert("Hi"+(i+1)+name_full)
+    }
+    name_full = 900
+    alert(typeof(name_full))
+    name_full = name_full + 1
+    console.log(name_full)
+    console.log(typeof(name_full))
 }
